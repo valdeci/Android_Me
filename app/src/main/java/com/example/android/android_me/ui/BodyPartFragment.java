066dcfad62a5dd0ab57989a6518798fce49d9fc2
Valdeci <valdecipti@gmail.com>
2017-11-15 16:19:33 -0200

    Create Body Part fragment

---
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -0,0 +1,33 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+/**
+ * Created by gemeos_valdeci on 15/11/2017.
+ */
+
+public class BodyPartFragment extends Fragment {
+
+    public BodyPartFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
+        ImageView img = (ImageView) rootView.findViewById(R.id.img_body_part);
+
+        img.setBackgroundResource(AndroidImageAssets.getHeads().get(0));
+
+        return rootView;
+    }
+}
